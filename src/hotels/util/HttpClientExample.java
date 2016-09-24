e7cdc3fdadad8f3115d78827cebca8467f06abe4
zabilal@Nova Systems <NOVA@DESKTOP-IE5NV3D>
2016-09-24 11:26:30 +0100

    Some UI for the Administrative Module & Util Classes

--- src/hotels/util/HttpClientExample.java
+++ src/hotels/util/HttpClientExample.java
@@ -22,6 +22,7 @@ import org.apache.http.message.BasicNameValuePair;
 public class HttpClientExample {
 
 	private final String USER_AGENT = "Mozilla/5.0";
+        private Navigator nav = new Navigator();
 
 	public static void main(String[] args) throws Exception {
 
@@ -40,28 +41,31 @@ public class HttpClientExample {
 
 		String url = "http://www.google.com/search?q=developer";
 
-		HttpClient client = new DefaultHttpClient();
-		HttpGet request = new HttpGet(url);
-                
-		// add request header
-		request.addHeader("User-Agent", USER_AGENT);
+//		HttpClient client = new DefaultHttpClient();
+//		HttpGet request = new HttpGet(url);
+//                
+//		// add request header
+//		request.addHeader("User-Agent", USER_AGENT);
+//
+//		HttpResponse response = client.execute(request);
+//
+//		System.out.println("\nSending 'GET' request to URL : " + url);
+//		System.out.println("Response Code : " +
+//                       response.getStatusLine().getStatusCode());
+//
+//		BufferedReader rd = new BufferedReader(
+//                       new InputStreamReader(response.getEntity().getContent()));
+//
+//		StringBuffer result = new StringBuffer();
+//		String line = "";
+//		while ((line = rd.readLine()) != null) {
+//			result.append(line);
+//		}
+//
+//		System.out.println(result.toString());
 
-		HttpResponse response = client.execute(request);
-
-		System.out.println("\nSending 'GET' request to URL : " + url);
-		System.out.println("Response Code : " +
-                       response.getStatusLine().getStatusCode());
-
-		BufferedReader rd = new BufferedReader(
-                       new InputStreamReader(response.getEntity().getContent()));
-
-		StringBuffer result = new StringBuffer();
-		String line = "";
-		while ((line = rd.readLine()) != null) {
-			result.append(line);
-		}
-
-		System.out.println(result.toString());
+                String result = nav.getEncode(url, null);
+                System.out.println("Result : " + result);
 
 	}
 
@@ -70,37 +74,40 @@ public class HttpClientExample {
 
 		String url = "https://selfsolve.apple.com/wcResults.do";
 
-		HttpClient client = new DefaultHttpClient();
-		HttpPost post = new HttpPost(url);
-
-		// add header
-		post.setHeader("User-Agent", USER_AGENT);
-
+//		HttpClient client = new DefaultHttpClient();
+//		HttpPost post = new HttpPost(url);
+//
+//		// add header
+//		post.setHeader("User-Agent", USER_AGENT);
+//
 		List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
 		urlParameters.add(new BasicNameValuePair("sn", "C02G8416DRJM"));
 		urlParameters.add(new BasicNameValuePair("cn", ""));
 		urlParameters.add(new BasicNameValuePair("locale", ""));
 		urlParameters.add(new BasicNameValuePair("caller", ""));
 		urlParameters.add(new BasicNameValuePair("num", "12345"));
+//
+//		post.setEntity(new UrlEncodedFormEntity(urlParameters));
+//
+//		HttpResponse response = client.execute(post);
+//		System.out.println("\nSending 'POST' request to URL : " + url);
+//		System.out.println("Post parameters : " + post.getEntity());
+//		System.out.println("Response Code : " +
+//                                    response.getStatusLine().getStatusCode());
+//
+//		BufferedReader rd = new BufferedReader(
+//                        new InputStreamReader(response.getEntity().getContent()));
+//
+//		StringBuffer result = new StringBuffer();
+//		String line = "";
+//		while ((line = rd.readLine()) != null) {
+//			result.append(line);
+//		}
+//
+//		System.out.println(result.toString());
 
-		post.setEntity(new UrlEncodedFormEntity(urlParameters));
-
-		HttpResponse response = client.execute(post);
-		System.out.println("\nSending 'POST' request to URL : " + url);
-		System.out.println("Post parameters : " + post.getEntity());
-		System.out.println("Response Code : " +
-                                    response.getStatusLine().getStatusCode());
-
-		BufferedReader rd = new BufferedReader(
-                        new InputStreamReader(response.getEntity().getContent()));
-
-		StringBuffer result = new StringBuffer();
-		String line = "";
-		while ((line = rd.readLine()) != null) {
-			result.append(line);
-		}
-
-		System.out.println(result.toString());
+                String result = nav.postEncode(url, urlParameters);
+                System.out.println("Result : " + result);
 
 	}
 
