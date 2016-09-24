54537abe431c5e998f9862248c0bf11c3e615c6a
Olyjosh <mac@fe80:0:0:0:f84e:fff:fe26:f74d%9>
2016-09-24 14:49:34 +0100

    Little Touch on Navigator

--- src/hotels/util/Storage.java
+++ src/hotels/util/Storage.java
@@ -6,8 +6,24 @@ package hotels.util;
  */
 public class Storage {
     
+    public static String auth_token ="";
+
+    public static String getAuth_token() {
+        return auth_token;
+    }
+
+    public static void setAuth_token(String auth_token) {
+        Storage.auth_token = auth_token;
+    }
     
+    
+            
+            
+            
     public static String floors(){
         return null;
     }
+    
+    
+    
 }
