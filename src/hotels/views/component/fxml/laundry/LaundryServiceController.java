0140aa3fa236b54f138bff6e9c597ebc62c6f983
Olyjosh <mac@fe80:0:0:0:f84e:fff:fe26:f74d%9>
2016-09-29 17:07:36 +0100

    Committing after merging. Just that I forget to

---
+++ src/hotels/views/component/fxml/laundry/LaundryServiceController.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotels.views.component.fxml.laundry;
+
+import hotels.Hotels;
+import hotels.util.Navigator2;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author mac
+ */
+public class LaundryServiceController implements Initializable {
+
+      
+    private Hotels app;
+    private Navigator2 nav;
+    public Hotels getApp() {
+        return app;
+    }
+
+    public void setApp(Hotels app) {
+        this.app = app;
+    }
+
+    public LaundryServiceController(Hotels app) {
+        this.app =app;
+        nav = new Navigator2(getApp().getMain());
+    }
+    
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
