ee1a62f92b41a0e5b9c62d6bd6efc3077bfb5c12
Olyjosh <mac@fe80:0:0:0:f84e:fff:fe26:f74d%9>
2016-10-11 04:17:46 +0100

    Faulty Charger 2

--- src/hotels/views/component/fxml/restaurant/controller/NewFood.java
+++ src/hotels/views/component/fxml/restaurant/controller/NewFood.java
@@ -9,6 +9,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -17,6 +18,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -81,6 +84,22 @@ public class NewFood implements Initializable {
 
     @FXML private void dragDropped(DragEvent e){
         System.out.println("Drag DROPED");
+        
+        Dragboard db = e.getDragboard();
+        boolean success = false;
+        if (db.hasFiles()) {
+            success = true;
+            filePath = null;
+            File file = db.getFiles().get(0);
+            filePath = file.getAbsolutePath();
+            try {
+                imgV.setImage(new Image(new FileInputStream(file)));
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(NewFood.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        e.setDropCompleted(success);
+        e.consume();
     }
     
     @FXML private void dragDone(DragEvent e){
@@ -99,10 +118,60 @@ public class NewFood implements Initializable {
         System.out.println("DONE");
     }
  
+
+  @FXML private void mouseDragDropped(final DragEvent e) {
+        final Dragboard db = e.getDragboard();
+        boolean success = false;
+        if (db.hasFiles()) {
+            success = true;
+            // Only get the first file from the list
+            final File file = db.getFiles().get(0);
+            Platform.runLater(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(file.getAbsolutePath());
+                    try {
+
+                        Image img = new Image(new FileInputStream(file.getAbsolutePath()));  
+                        imgV.setImage(img);
+                        
+                    } catch (FileNotFoundException ex) {
+                        Logger.getLogger(NewFood.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            });
+        }
+        e.setDropCompleted(success);
+        e.consume();
+    }
+ 
+    @FXML private  void mouseDragOver(final DragEvent e) {
+        final Dragboard db = e.getDragboard();
+ 
+        final boolean isAccepted = db.getFiles().get(0).getName().toLowerCase().endsWith(".png")
+                || db.getFiles().get(0).getName().toLowerCase().endsWith(".jpeg")
+                || db.getFiles().get(0).getName().toLowerCase().endsWith(".jpg");
+ 
+        if (db.hasFiles()) {
+            if (isAccepted) {
+                imgStack.setStyle("-fx-border-color: red;"
+              + "-fx-border-width: 5;"
+              + "-fx-background-color: #C6C6C6;"
+              + "-fx-border-style: solid;");
+                e.acceptTransferModes(TransferMode.COPY);
+            }
+        } else {
+            e.consume();
+        }
+    }
+     
+     
+     
+     
      
      private String filePath;
      private void pickFile(){
-        File f = filechoose(new FileChooser.ExtensionFilter("Images - Only jpeg and png are supported", "jpeg", "jpg","png"));
+        File f = filechoose(new FileChooser.ExtensionFilter("Images - Only jpeg and png are supported", "*.jpeg", "*.jpg","*.spng"));
         if(f!=null){
             try {
                 imgV.setImage(new Image(new FileInputStream(f)));
